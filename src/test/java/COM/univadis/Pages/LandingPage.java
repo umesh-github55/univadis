ebb554f6de7563cd26993e53734ebdf7ebfe5228
UmeshSutar <sutarumesh71@gmail.com>
2021-08-12 06:40:38 -0700

    Add all pages

--- src/test/java/COM/univadis/Pages/LandingPage.java
+++ src/test/java/COM/univadis/Pages/LandingPage.java
@@ -1,5 +1,13 @@
 package COM.univadis.Pages;
 
-public class LandingPage {
+import org.openqa.selenium.WebDriver;
 
+public class LandingPage extends NavBarCompoent {
+
+	public LandingPage(WebDriver wd) {
+		super(wd);
+		// TODO Auto-generated constructor stub
+	}
+
+	
 }
